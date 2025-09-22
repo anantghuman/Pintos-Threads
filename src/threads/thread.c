a64fdc59eb0244da77e69a56b0044c18c1615ef6
Soham Gupta <sgupta03@cs.utexas.edu>
2025-09-22 17:46:09 -0500

    nt working

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -257,7 +257,7 @@ const char *thread_name (void) { return thread_current ()->name; }
 /* Returns the running thread.
    This is running_thread() plus a couple of sanity checks.
    See the big comment at the top of thread.h for details. */
-struct thread *thread_current (void)
+struct thread *thread_current (void) 
 {
   struct thread *t = running_thread ();
 
